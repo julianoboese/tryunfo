d2e4618ec2d5b62ebc408e2ee82799c5a775aa53
Juliano Boese <juliano.boese@gmail.com>
2022-01-28 17:36:23 -0300

    Requisito 4 - Cria o preview da carta que está sendo inserida no form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,56 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      attr1: 0,
+      attr2: 0,
+      attr3: 0,
+      image: '',
+      rare: 'normal',
+      trunfo: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    this.setState({
+      [target.name]: target.type === 'checkbox' ? target.checked : target.value,
+    });
+  }
+
   render() {
+    const { name, description, attr1, attr2, attr3, image, rare, trunfo } = this.state;
     return (
       <>
         <div>
           <h1>Tryunfo</h1>
         </div>
-        <Form />
-        <Card />
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+        />
       </>
     );
   }
